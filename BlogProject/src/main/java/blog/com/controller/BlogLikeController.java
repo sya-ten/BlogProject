d882b18ca1e830c0f77adbbced7da9a8476f391e
xietianMctJapan <708825737@qq.com>
2025-05-20 15:44:55 +0900

    update

--- BlogProject/src/main/java/blog/com/controller/BlogLikeController.java
+++ BlogProject/src/main/java/blog/com/controller/BlogLikeController.java
@@ -8,56 +8,53 @@ import java.nio.file.StandardCopyOption;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
 import blog.com.model.dao.AccountDao;
 import blog.com.model.dao.BlogDao;
-import blog.com.model.dao.CommentDao;
+import blog.com.model.dao.BlogLikeDao;
 import blog.com.model.entity.Account;
 import blog.com.model.entity.Blog;
+import blog.com.model.entity.BlogLike;
 import blog.com.model.entity.BlogModel;
-import blog.com.model.entity.Comment;
-import blog.com.model.entity.CommentModel;
-import blog.com.model.entity.RowModel;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-public class CommentRegisterController {
+public class BlogLikeController {
 	// DAOを自動注入（DB操作用）
 	@Autowired
 	private BlogDao blogDao;
 	// DAOを自動注入（DB操作用）
 	@Autowired
-	private AccountDao accountDao;
+	private AccountDao AccountDao;
 	// DAOを自動注入（DB操作用）
 	@Autowired
-	private CommentDao commentDao;
-	
-	// ブログ登録処理
-	@PostMapping("/commentRegister")
-	public String commentRegister(@RequestParam("content") String content,
-								  @RequestParam("blogId") Long blogId,
-							      HttpSession session) throws IOException {
-		//アカウント情報がないと、ログイン画面に進む
-		Account account = (Account)session.getAttribute("account");
-		if (account==null) {
-			return "redirect:/login";
+	private BlogLikeDao blogLikeDao;
+
+	// ブログ編集画面を表示
+	@PostMapping("/blogLike")
+	@ResponseBody
+	public void PostBlogLike(@RequestParam("liker_id") Long liker_id,
+							 @RequestParam("blog_id") Long blog_id,
+							 @RequestParam("add") Boolean add,
+							 HttpSession session,
+						   	 Model model) {
+		BlogLike blogLike = new BlogLike(liker_id, blog_id);
+		//いいね追加モード
+		if (add) {
+			blogLikeDao.save(blogLike);
 		}
-		
-		Comment comment = new Comment(content, account.getAccountId(), blogId);
-		//コメント情報をテーブルに保存する
-		commentDao.save(comment);
-		return "redirect:/blogList";
+		//いいね削除モード
+		else {
+			blogLikeDao.deleteByBlogIdAndLikerId(blog_id, liker_id);
+		}
 	}
 }
